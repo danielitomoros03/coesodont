d71582add2a658cd88db1d58cf627aa44f5c58fe
Daniel Moros <danielito.moros03@gmail.com>
2023-05-29 10:42:49 -0400

    Update seeds.rb
    Se comentaron las líneas que en próximas facultades no harán falta.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,36 +18,36 @@ p '      Creado Primer Admin!    '.center(200, '=') if Admin.create!(user_id: us
 
 p '      Creada Escuela!    '.center(200, '=') if escuela = School.create(code: 'ODONT', name: 'Escuela de Odontología', faculty: odont)
 
-p '      Creados Primeros Tipos de Períodos!    '.center(200, '=') if PeriodType.create([{code: 'I', name: 'Primero'}, {code: 'II', name: 'Segundo'}, {code: 'U', name: 'Único'}, {code: 'E', name: 'Especial (Intensivo)'}])
+# p '      Creados Primeros Tipos de Períodos!    '.center(200, '=') if PeriodType.create([{code: 'I', name: 'Primero'}, {code: 'II', name: 'Segundo'}, {code: 'U', name: 'Único'}, {code: 'E', name: 'Especial (Intensivo)'}])
 
-p '      Creados Primeros Períodos!    '.center(200, '=') if Period.create([{year: 2022, period_type_id: 1}, {year: 2022, period_type_id: 2}])
+# p '      Creados Primeros Períodos!    '.center(200, '=') if Period.create([{year: 2022, period_type_id: 1}, {year: 2022, period_type_id: 2}])
 
-p '      Creado Primer Plan de Estudio!    '.center(200, '=') if StudyPlan.create(code: 'B001', name: 'Arquitecto', school_id: School.first.id)
+# p '      Creado Primer Plan de Estudio!    '.center(200, '=') if StudyPlan.create(code: 'B001', name: 'Arquitecto', school_id: School.first.id)
 
-p '      Creadas Primeras Asignturas Madres!    '.center(200, '=') if Area.create([{school_id: escuela.id, name: "Diseño Arquitectónico"},
-{school_id: escuela.id, name: "Métodos"},
-{school_id: escuela.id, name: "Tecnología"},
-{school_id: escuela.id, name: "Historia y Crítica"},
-{school_id: escuela.id, name: "Acondicionamiento Ambiental"},
-{school_id: escuela.id, name: "Estudios Urbanos"}])
+# p '      Creadas Primeras Asignturas Madres!    '.center(200, '=') if Area.create([{school_id: escuela.id, name: "Diseño Arquitectónico"},
+# {school_id: escuela.id, name: "Métodos"},
+# {school_id: escuela.id, name: "Tecnología"},
+# {school_id: escuela.id, name: "Historia y Crítica"},
+# {school_id: escuela.id, name: "Acondicionamiento Ambiental"},
+# {school_id: escuela.id, name: "Estudios Urbanos"}])
 
-area = Area.where(name: "DISEÑO ARQUITECTÓNICO").first
+# area = Area.where(name: "DISEÑO ARQUITECTÓNICO").first
 
-Area.create([{school_id: escuela.id, name: "Expresión", parent_area_id: area.id},
-{school_id: escuela.id, name: "Teoría de la Arquitectura", parent_area_id: area.id},
-{school_id: escuela.id, name: "Diseño Arquitectónico Sub", parent_area_id: area.id}])
+# Area.create([{school_id: escuela.id, name: "Expresión", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Teoría de la Arquitectura", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Diseño Arquitectónico Sub", parent_area_id: area.id}])
 
-area = Area.where(name: "MÉTODOS").first
+# area = Area.where(name: "MÉTODOS").first
 
-Area.create([{school_id: escuela.id, name: "Matemáticas", parent_area_id: area.id},
-{school_id: escuela.id, name: "Investigación y Creatividad", parent_area_id: area.id},
-{school_id: escuela.id, name: "Informática", parent_area_id: area.id}])
+# Area.create([{school_id: escuela.id, name: "Matemáticas", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Investigación y Creatividad", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Informática", parent_area_id: area.id}])
 
-area = Area.where(name: "TECNOLOGÍA").first
+# area = Area.where(name: "TECNOLOGÍA").first
 
-Area.create([{school_id: escuela.id, name: "Construcción", parent_area_id: area.id},
-{school_id: escuela.id, name: "Instalaciones", parent_area_id: area.id},
-{school_id: escuela.id, name: "Estructura", parent_area_id: area.id}])
+# Area.create([{school_id: escuela.id, name: "Construcción", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Instalaciones", parent_area_id: area.id},
+# {school_id: escuela.id, name: "Estructura", parent_area_id: area.id}])
 
 Bank.create([{code: "0006", name: "Banco de Coro"},
 {code: "0007", name: "Banfoandes"},
